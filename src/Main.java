934d14de347ae0286bbce5d1ca45181388f6cbb0
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-04-22 02:07:38 +0200

    adding validation

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,44 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthly_interest_rate = 0;
+        int monthly_period = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true){
+            System.out.print("Principal ($1k - $1M): ");
+            int input = scanner.nextInt();
+            if (input >= 1000 && input <= 1000_000){
+                principal = input;
+                break;
+            }
 
-        System.out.print("Annual Interest Rate: ");
-        float annual_interest_rate = scanner.nextFloat();
-        float monthly_interest_rate = annual_interest_rate / MONTHS_IN_YEAR /  PERCENT;
+            System.out.println("Please enter principal between 1000 and 1000000");
+        }
 
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
-        int monthly_period = period * MONTHS_IN_YEAR;
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            float annual_interest_rate = scanner.nextFloat();
+            if (annual_interest_rate > 0 && annual_interest_rate <= 30){
+                monthly_interest_rate = annual_interest_rate / MONTHS_IN_YEAR /  PERCENT;
+                break;
+            }
 
+            System.out.println("Enter value greater than 0 and less then or equal to 30");
+        }
+
+        while (true){
+            System.out.print("Period (Years): ");
+            int period = scanner.nextByte();
+            if (period > 0 && period <= 30){
+                monthly_period = period * MONTHS_IN_YEAR;
+                break;
+            }
+
+            System.out.println("Enter value between 0 and 30");
+        }
 
         double mortgage = principal * (
                 (monthly_interest_rate * Math.pow(1 + monthly_interest_rate, monthly_period))
